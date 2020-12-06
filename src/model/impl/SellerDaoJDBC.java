81315ddeb2ca366ad3a765cb141584db45706467
Thiago <thiago.silveira@projuris.com.br>
2020-12-06 18:43:47 -0300

    Adding SellerDaoJDBC and DaoFactory

---
+++ src/model/impl/SellerDaoJDBC.java
@@ -0,0 +1,34 @@
+package model.impl;
+
+import model.dao.SellerDao;
+import model.entities.Seller;
+
+import java.util.List;
+
+public class SellerDaoJDBC implements SellerDao {
+
+    @Override
+    public void insert(Seller objeto) {
+
+    }
+
+    @Override
+    public void update(Seller objeto) {
+
+    }
+
+    @Override
+    public void daleteById(Integer id) {
+
+    }
+
+    @Override
+    public Seller findById(Integer id) {
+        return null;
+    }
+
+    @Override
+    public List<Seller> findAll() {
+        return null;
+    }
+}
